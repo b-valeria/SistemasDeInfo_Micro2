7a720a1899c86bb3da0c6768679108097e308308
Valeria V, Badell S. <161253752+b-valeria@users.noreply.github.com>
2025-03-11 10:07:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { UserProvider } from './context/UserContext'
+import { UserProvider } from './Context/UserContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

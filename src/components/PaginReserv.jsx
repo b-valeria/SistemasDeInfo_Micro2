4cf1d419bb66e07d690279b78e74366333a83ebc
Alejoaho-17 <alejo.hernandez@correo.unimet.edu.ve>
2025-03-15 16:22:06 -0400

    Commit | Avance en desarrollo Pag. Reserv

--- src/components/PaginReserv.jsx
+++ src/components/PaginReserv.jsx
@@ -1,38 +1,58 @@
 "use client";
 import React from "react";
-import styles from "./MainContainer.module.css";
-
-
+import styles from "./PaginReserv.module.css";
 
 function PaginReserva() {
-
-
-
-  let rutas = ['Sabas-Nieves','Pico-Naiguata']
+  let rutas = ["Sabas-Nieves", "Pico-Naiguata"];
+  let actividades = ["Senderismo", "Fotografia"];
   return (
     <main className={styles.mainContainer}>
-      
+      <form
+        className={styles.cardForm}
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(event.target.elements.ruta.value);
+        }}
+      >
 
+          <label className = {styles.labelContainer} htmlFor="Fecha">
+            {" "}
+            Fecha <input name="Fecha" id="Fecha" type="date" />{" "}
+          </label>
+          <label className = {styles.labelContainer} htmlFor="Hora">
+            {" "}
+            Hora <input name="Hora" id="Hora" type="time" />{" "}
+          </label>
 
-      <form onSubmit={(event) => {event.preventDefault(); console.log(event.target.elements.ruta.value)}}>
-          <div>
+          <div className = {styles.labelContainer}>
             <p>
-              Seleccionar una ruta 
+              Selecciona una ruta: 
             </p>
-            <div>
-              {rutas.map((ruta)=><label htmlFor = {ruta}> {ruta}  <input name = "ruta" type="radio" id = {ruta} value = {ruta} />  </label>)}
-            </div>
-
+            {rutas.map((ruta) => (
+              <label className = {styles.checkBoxLabel} htmlFor={ruta}>
+                {" "}
+                {ruta} <input name="ruta" type="radio" id={ruta} value={ruta} />{" "}
+              </label>
+            ))}
           </div>
-          <input type = "submit" value = {"reservar"}/>
+
+          <label className = {styles.labelContainer} htmlFor="Actividades">
+            {" "}
+            Actividades{" "}
+            <select  name="Actividades" id="Actividades">
             
-          
+              {actividades.map((actividad) => (
+                <option className = {styles.selectOption} value={actividad}> {actividad} </option>
+              ))}
+              
+            </select>{" "}
+          </label>
+        
 
+        <input type="submit" value={"reservar"} />
       </form>
-
     </main>
   );
 }
 
 export default PaginReserva;
-

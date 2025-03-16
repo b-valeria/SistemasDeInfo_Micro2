036833662ea3138e625d8e12707d8540b1b62bd0
b-valeria <b.valeria@correo.unimet.edu.ve>
2025-03-15 21:01:07 -0400

    haciendo el size super grande

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,20 +7,20 @@ export default function Home() {
         {
             title: "Raíces Unimetanas",
             text: "En nuestra agencia de excursiones, nos enorgullece contar con un equipo de guías apasionados y altamente capacitados, todos ellos egresados de la Universidad Metropolitana. Gracias a la formación integral que ofrece esta prestigiosa institución, nuestros guías no solo están equipados con conocimientos académicos, sino también con una profunda comprensión del entorno natural y cultural que nos rodea.",
-            width: 200,
-            height: 100, 
+            width: 600,
+            height: 500, 
         },
         {
             title: "Nosotros",
             text: "Nos dedicamos a ofrecer experiencias únicas en el Parque Nacional El Ávila, en Caracas. Con años de experiencia en ecoturismo, nuestro objetivo es conectar a los visitantes con la belleza natural y la rica biodiversidad de esta emblemática montaña. Nos comprometemos a brindar excursiones seguras, educativas y memorables, fomentando el respeto por el medio ambiente y la cultura local. ",
-            width: 200,
-            height: 100,
+            width: 600,
+            height: 500,
         },
         {
             title: "Amantes del Ávila",
             text: "El Ávila, también conocido como Waraira Repano, es un emblemático cerro que se alza sobre Caracas, Venezuela. Su historia se remonta a tiempos precolombinos, cuando las comunidades indígenas lo consideraban sagrado. Durante la colonización española, el Ávila fue testigo del crecimiento de la ciudad y se convirtió en un símbolo de identidad para los caraqueños.",
-            width: 200,
-            height: 100,
+            width: 600,
+            height: 500,
         },
     ];
     

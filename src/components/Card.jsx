06cb0ec9bf6864aa3e04d34deafc983a19beecb1
b-valeria <b.valeria@correo.unimet.edu.ve>
2025-03-15 20:25:07 -0400

    Cambié cards para que cuando la vayan a utilizar reciba titulo y texto, y corregí algunos acentos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card({ title, text }) {
     return (
         <div className="rounded-2xl h-72 w-72 bg-gradient-to-br from-gray-200/30 to-gray-300/30 backdrop-blur-md border border-gray-200/50 pt-2 px-5">
-            <h1 className="text-center text-lg font-bold mb-10">Raices Unimetanas</h1>
+            <h1 className="text-center text-lg font-bold mb-10">{title}</h1>
             <p className="text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam temporibus neque molestiae quas odio soluta nesciunt quos alias dolorem. Dolorem vero, eos h...
+                {text}
             </p>
         </div>
     );
 }
+

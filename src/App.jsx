3df1e787121bf80cb9bcf4efbaa671f14e52189c
ChristianR10 <christian.ramos@correo.unimet.edu.ve>
2025-03-19 02:51:43 -0400

    merge raro antes de dormir

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from "./Pages/Login"
 import Register from "./Pages/Register"
 import Destinos from "./components/Destinos";
 import DestinationDetail from './components/DestinationDetail'
-<<<<<<< HEAD
 import Novedades from './Pages/Novedades';
 
 import SabasNieves from './Pages/DestinosDetalles/SabasNieves';
@@ -14,13 +13,8 @@ import ElBanquito from './Pages/DestinosDetalles/ElBanquito';
 import PicoNaiguata from './Pages/DestinosDetalles/PicoNaiguata';
 import PiedraIndio from './Pages/DestinosDetalles/PiedraIndio';
 import PicoOriental from './Pages/DestinosDetalles/PicoOriental';
-=======
-<<<<<<< HEAD
+
 import PaginReserva from "./components/PaginReserv"
-=======
-import Novedades from './Pages/Novedades';
->>>>>>> f376a7daea2cbb23918b2827c888374e26bf4fd6
->>>>>>> 639656033b863ed7ed0a5e70a72598ed6ad06146
 
 
 function App() {
@@ -38,7 +32,6 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/destinos" element={<Destinos />} />
           <Route path='/destinos/:destino' element={<DestinationDetail/>} />
-<<<<<<< HEAD
           <Route path="/novedades" element={<Novedades />} /> 
 
           <Route path="/SabasNieves" element={<SabasNieves />} />
@@ -48,13 +41,8 @@ function App() {
           <Route path="/PiedraIndio" element={<PiedraIndio />} />
           <Route path="/PicoOriental" element={<PicoOriental />} />
 
-=======
-<<<<<<< HEAD
+
           <Route path='/destinos/:destino/reservar' element={<PaginReserva/>} />
-=======
-          <Route path="/novedades" element={<Novedades />} /> 
->>>>>>> f376a7daea2cbb23918b2827c888374e26bf4fd6
->>>>>>> 639656033b863ed7ed0a5e70a72598ed6ad06146
         </Route>
 
 

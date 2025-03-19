d38d5634f04e34ee4d79a258675b9f6ed6fdaa08
ChristianR10 <christian.ramos@correo.unimet.edu.ve>
2025-03-19 02:55:12 -0400

    arreglado el merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{use} from "react";
 import styles from "./MainContainer.module.css";
 import { Link } from "react-router";
@@ -37,44 +36,4 @@ function Header() {
   );
 }
 
-=======
-import React,{use} from "react";
-import styles from "./MainContainer.module.css";
-import { Link } from "react-router";
-
-function Header() {
-
-
-  return (
-    <header className={styles.header}>
-      <img
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/af5ced3c35b122040f4ac92ba7f1b1e9a1a8bb6d"
-        alt="Logo"
-        className={styles.logo}
-      />
-      <nav className={styles.navLinks}>
-        <a href="#" className={styles.navItem}>
-          Home
-        </a>
-        <a href="#" className={styles.navItem}>
-          Destinos
-        </a>
-        <a href="#" className={styles.navItem}>
-          Novedades
-        </a>
-        <a href="#" className={styles.navItem}>
-          Contacto
-        </a>  
-        
-        
-    
-
-
-  
-      </nav>
-    </header>
-  );
-}
-
->>>>>>> 639656033b863ed7ed0a5e70a72598ed6ad06146
 export default Header;
